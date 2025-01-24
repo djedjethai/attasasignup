92de1fe18550069fec435059a63e319261e8cf7c
Jerome Bidault <djedje.thai.ok@gmail.com>
2025-01-24 19:24:27 +0700

    Changed implementation, return a PDF

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ from reportlab.pdfbase.ttfonts import TTFont
 import os
 from reportlab.pdfbase import pdfmetrics
 
-image_path = './data/images.png'
+from utility import replace_title, may_empty
+
+image_path = './data/picture.png'
 
 # Load the Excel file
 excel_file = "./data/responses.xlsx"  # Replace with your file name
 data = pd.read_excel(excel_file)
 
 # Register the Thai font if needed
-pdfmetrics.registerFont(TTFont('THSarabun', './THSarabun/THSarabun-New-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('THSarabun', './fonts/THSarabun-New-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('DejaVuSans', './fonts/DejaVuSans.ttf'))
 
 # Function to read template from a file
 def load_template(template_file):
@@ -23,55 +26,118 @@ def load_template(template_file):
 template_file = "./forms/formTxt.txt"  # Replace with the path to your template file
 template = load_template(template_file)
 
+placeholder = ''
+
 # Function to replace placeholders
 def generate_form(row, template):
     form = template
     for col in row.index:
         print(col)
-        placeholder = ''
         if col.strip() == 'เพศ':
-            if row[col].strip() == 'นางสาว':
-                placeholder = '$02'
-                print(f"Column: {col}, Value: {row[col]}")
-                print('grrrrr in pet')
-                # form = form.replace(placeholder, str(row[col]) if pd.notna(row[col]) else "")
-                form = form.replace(placeholder, 'X' if pd.notna(row[col]) else "")
+            form = replace_title(form, row[col])
+        elif col.strip() == 'ชื่อ':
+                placeholder = '$2'
+                form = form.replace(placeholder, str(row[col]))
+        elif col.strip() == 'นามสกุล':
+                placeholder = '$3'
+                form = form.replace(placeholder, str(row[col]))
+        elif col.strip() == 'วัน/เดือน/ปี (พ.ศ.) เกิด':
+                placeholder = '$4'
+                form = form.replace(placeholder, str(row[col]))
+        elif col.strip() == 'ที่อยู่ปัจจุบัน เลขที่':
+                placeholder = '$5'
+                form = form.replace(placeholder, str(row[col]))
+        elif col.strip() == 'หมู่ที่':
+                placeholder = '$6'
+                form = may_empty(form, row[col], placeholder)
+        elif col.strip() == 'หมู่บ้าน':
+                placeholder = '$7'
+                form = may_empty(form, row[col], placeholder)
+        elif col.strip() == 'ซอย':
+                placeholder = '$8'
+                form = may_empty(form, row[col], placeholder)
+        elif col.strip() == 'ถนน':
+                placeholder = '$9'
+                form = may_empty(form, row[col], placeholder)
+        elif col.strip() == 'แขวง':
+                placeholder = '$10'
+                form = form.replace(placeholder, str(row[col]))
+        elif col.strip() == 'เขต':
+                placeholder = '$11'
+                form = form.replace(placeholder, str(row[col]))
+        elif col.strip() == 'จังหวัด':
+                placeholder = '$12'
+                form = form.replace(placeholder, str(row[col]))
+        elif col.strip() == 'รหัสไปรษณีย์':
+                placeholder = '$13'
+                form = form.replace(placeholder, str(row[col]))
+        elif col.strip() == 'รุ่นที่': # krou samathi
+                placeholder = '$19'
+                form = may_empty(form, row[col], placeholder)
+        elif col.strip() == 'เลขสาขา':
+                placeholder = '$-2'
+                form = may_empty(form, row[col], placeholder)
+        elif col.strip() == 'เบอร์โทรศัพท์ (รูปแบบ xxx-xxx-xxxx)':
+                placeholder = '$14'
+                form = form.replace(placeholder, str(row[col]))
+        elif col.strip() == 'กรณีฉุกเฉินติดต่อ':
+                placeholder = '$17'
+                form = form.replace(placeholder, str(row[col]))
+        elif col.strip() == 'เบอร์โทรศัพท์ผู้ติดต่อฉุกเฉิน (รูปแบบ xxx-xxx-xxxx)':
+                placeholder = '$18'
+                form = form.replace(placeholder, str(row[col]))
         elif col.strip() == 'กรุณาตรวจสอบคำตอบของคุณก่อนที่จะส่ง':
             print('.............')
             # add a pic
 
-
     # print("Generated form:", form)
     return form
 
+
 def create_pdf(content, output_path, image_path=None):
+    line_spacing = 2
+
     c = canvas.Canvas(output_path, pagesize=letter)
     width, height = letter
     c.setFont("THSarabun", 18)  # Use the Thai font here
+    # c.setFont("DejaVuSans", 18) 
     lines = content.split("\n")
 
     y_position = height - 40  # Starting position from the top
     
     for line in lines:
-        c.drawString(40, y_position, line)
-        y_position -= 14  # Move down for the next line
+        y_position -= line_spacing
+
+        if 'X' in line:
+            parts = line.split('X')
+            x_position = 40
+            for part in parts[:-1]:
+                c.drawString(x_position, y_position, part)
+                x_position += c.stringWidth(part, "THSarabun", 18)
+                c.setFont("DejaVuSans", 18)
+                c.drawString(x_position, y_position, '☒')
+                x_position += c.stringWidth('☒', "DejaVuSans", 18)
+                c.setFont("THSarabun", 18)
+            c.drawString(x_position, y_position, parts[-1])
+        else:
+            c.drawString(40, y_position, line)
         
+                
         if 'ธรรมศึกษาชั้นตรี' in line:
-            picture_x = 500  # X position of image (adjust as needed)
-            picture_y = y_position - 60  # Y position (adjust as needed)
+            picture_x = 480  # X position of image (adjust as needed)
+            picture_y = y_position - 75  # Y position (adjust as needed)
             c.drawImage(image_path, picture_x, picture_y, width=100, height=100)  # Adjust size and position
             # y_position -= 120  # Adjust space for the image
-            y_position = picture_y + 30  # Adjust y_position to account for the height of the image
+            y_position = picture_y + 75  # Adjust y_position to account for the height of the image
 
         if 'ธรรมศึกษาชั้นเอก' in line:
-            picture_x = 500  # X position of image (adjust as needed)
-            picture_y = y_position - 60  # Y position (adjust as needed)
+            picture_x = 480  # X position of image (adjust as needed)
+            picture_y = y_position - 75  # Y position (adjust as needed)
             c.drawImage(image_path, picture_x, picture_y, width=100, height=100)  # Adjust size and position
             # y_position -= 120  # Adjust space for the image
-            y_position = picture_y + 60  # Adjust y_position to account for the height of the image
+            y_position = picture_y + 75  # Adjust y_position to account for the height of the image
 
-
-
+        y_position -= 14
         
         if y_position < 40:  # Check if we're at the bottom of the page
             c.showPage()
--- utility.py
+++ utility.py
@@ -1,35 +1,36 @@
 import pandas as pd
 
-def may_empty(paragraph, value, placeholder):
+def may_empty(form, value, placeholder):
     if pd.isna(value):
-        paragraph.text = paragraph.text.replace(placeholder, '...............')
+        form = form.replace(placeholder, '.........')
     else:
-        paragraph.text = paragraph.text.replace(placeholder, str(value))
+        valueStr = str(value)
+        valueStr = valueStr.split('.')[0]
+        form = form.replace(placeholder, valueStr)
+    return form
 
-    return paragraph
-
-def replace_title(paragraph, gender_value):
+def replace_title(form, gender_value):
     # Define the placeholders to be replaced
     placeholders = ['$00', '$01', '$02', '$03']
      
     # Replace the correct placeholder with 'X' based on gender value
     if gender_value == 'นาย':
-        paragraph.text = paragraph.text.replace('$00', '☒')
+        form = form.replace('$00', 'X' if pd.notna(gender_value) else "")
         placeholders.remove('$00')
     elif gender_value == 'นาง':
-        paragraph.text = paragraph.text.replace('$01', '☒')
+        form = form.replace('$01', 'X' if pd.notna(gender_value) else "")
         placeholders.remove('$01')
     elif gender_value == 'นางสาว':
-        paragraph.text = paragraph.text.replace('$02', '☒')
+        form = form.replace('$02', 'X' if pd.notna(gender_value) else "")
         placeholders.remove('$02')
     else:
-        paragraph.text = paragraph.text.replace('$03', f"{gender_value}")
+        form = form.replace('$03', f"{gender_value}" if pd.notna(gender_value) else "")
         placeholders.remove('$03')
 
-    return clear_placeholders(paragraph, placeholders)
+    return clear_placeholders(form, placeholders)
 
-def clear_placeholders(paragraph, placeholders):
+def clear_placeholders(form, placeholders):
     # Remove placeholders that were not replaced yet
     for placeholder in placeholders:
-        paragraph.text = paragraph.text.replace(placeholder, '□')  # Replace the placeholder with a space
-    return paragraph
+        form = form.replace(placeholder, '□')
+    return form
